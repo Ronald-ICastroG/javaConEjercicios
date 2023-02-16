ac678df936fd7fb566ad2a1942edfb8b2ef9a2cc
Dell inspiron 14-3458 <ronaldivan.1210868@correo.uis.edu.co>
2023-02-15 23:50:54 -0500

    Ejercicios 9 y 10 inconclusos

---
+++ src/main/java/bucles/Ejercicio9.java
@@ -0,0 +1,20 @@
+package bucles;
+
+import java.util.Scanner;
+
+public class Ejercicio9 {
+    public static void main(String[] args) {
+        /* Realiza un programa que nos diga cuántos dígitos tiene un número introducido
+por teclado.*/
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Ingresa un número cualquiera: ");
+        int num=sc.nextInt();
+
+
+
+
+
+
+    }
+}
