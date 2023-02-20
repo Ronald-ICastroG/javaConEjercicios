19b5f0c670f55b65acddf8e5140707c4c367f664
Ronald-ICastroG_Acer <ronaldivan.1210868@correo.uis.edu.co>
2023-02-20 14:06:48 -0500

    ej9 bucles resuelto dígitos

--- src/main/java/bucles/Ejercicio9.java
+++ src/main/java/bucles/Ejercicio9.java
@@ -10,8 +10,13 @@ por teclado.*/
         Scanner sc=new Scanner(System.in);
         System.out.println("Ingresa un número cualquiera: ");
         int num=sc.nextInt();
-
-
+        int digitos=0;
+        int n1=num;
+        while (n1>=1){
+        n1=n1/10;
+        digitos++;
+        }
+        System.out.println("el número "+ num+" tiene "+digitos+" digitos");
 
 
 
